ebef351148afa3075968aa63c03790603d674523
shree675 <shreeteshm37@gmail.com>
2021-06-12 14:03:23 +0530

    modified files

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ require('dotenv').config({ silent: true });
 
 const app = require('./app');
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, 'client/build')));
-//   app.get('*', (req, res) => {
-//     res.sendfile(path.join((__dirname = 'client/build/index.html')));
-//   });
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join((__dirname = './build/index.html')));
+  });
+}
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
