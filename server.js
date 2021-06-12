8ade20c29867da08ea7fffccb0d431c4890a7e75
shree675 <shreeteshm37@gmail.com>
2021-06-12 16:53:54 +0530

    import express

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 #! /usr/bin/env node
 
 require('dotenv').config({ silent: true });
+const express = require('express');
 
 const app = require('./app');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, './build')));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join((__dirname = './build/index.html')));
+    res.sendFile(path.join((__dirname = 'build/index.html')));
   });
 }
 
