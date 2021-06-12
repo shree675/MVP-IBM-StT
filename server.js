0b9bc9aad0860c7efa2cbe382214f6b746a7b056
shree675 <shreeteshm37@gmail.com>
2021-06-12 13:47:58 +0530

    modified env

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ require('dotenv').config({ silent: true });
 
 const app = require('./app');
 
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('*', (req, res) => {
+//     res.sendfile(path.join((__dirname = 'client/build/index.html')));
+//   });
+// }
+
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
