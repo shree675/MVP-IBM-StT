f1623cb0325191c91d483d51471eff0b624f88af
Hirendra <stackwork87@gmail.com>
2021-06-14 14:48:31 +0530

    pulling changes

---
+++ build/precache-manifest.aac9a7f679967bff5421acd3e37e7946.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b693e39bc2f1543fca0375029fc8ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc9d8b3b12fa84c65214",
+    "url": "/static/css/main.8f37240a.chunk.css"
+  },
+  {
+    "revision": "80a6f80d303cc222a52d",
+    "url": "/static/js/2.1b4483d9.chunk.js"
+  },
+  {
+    "revision": "cb04fe505d3c6bebfd8dbf3e9e92539a",
+    "url": "/static/js/2.1b4483d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9d8b3b12fa84c65214",
+    "url": "/static/js/main.efcf9616.chunk.js"
+  },
+  {
+    "revision": "e2b2dcf7652e7edf011d",
+    "url": "/static/js/runtime-main.6faa723d.js"
+  },
+  {
+    "revision": "419c131b3daf232021c56ae4efbef6e5",
+    "url": "/static/media/google.419c131b.svg"
+  },
+  {
+    "revision": "7137286db3c9254ace982646907e65a3",
+    "url": "/static/media/twitter.7137286d.svg"
+  }
+]);
---
+++ src/components/HomePage/home-page.js
@@ -0,0 +1,127 @@
+import React, { Component, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import fire from '../../firebase';
+import './home-page.css';
+import ServiceContainer from '../ServiceContainer/ServiceContainer';
+import LoginPage from '../LoginPage/login-page';
+var CryptoJS = require('crypto-js');
+
+const HomePage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState(false);
+  const [user, setUser] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const pwd = CryptoJS.SHA3(password).toString();
+
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, pwd)
+      .catch((err) => {
+        switch (err.code) {
+          case 'auth/user-disabled':
+          case 'auth/user-not-found':
+            alert(err.message);
+            break;
+          case 'auth/wrong-password':
+            alert(err.message);
+        }
+        clearAll();
+      });
+
+    authListener();
+    clearAll();
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+
+    const pwd = CryptoJS.SHA3(password).toString();
+
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(email, pwd)
+      .catch((err) => {
+        clearAll();
+        switch (err.code) {
+          case 'auth/email-already-in-use':
+          case 'auth/invalid-email':
+            alert(err.message);
+            break;
+        }
+      });
+
+    authListener();
+    clearAll();
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    fire.auth().signOut();
+    authListener();
+  };
+
+  const authListener = (e) => {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  };
+
+  const clearAll = () => {
+    setEmail('');
+    setPassword('');
+  };
+
+  const onChangeUsername = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+    // this.setState({
+    //     email: e.target.value,
+    // });
+  };
+
+  const onChangePassword = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+    // this.setState({
+    //     password: e.target.value,
+    // });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  return (
+    <div>
+      {user !== null ? (
+        <ServiceContainer handleLogout={handleLogout} />
+      ) : (
+        <LoginPage
+          login={login}
+          setLogin={setLogin}
+          setEmail={setEmail}
+          setPassword={setPassword}
+          setUser={setUser}
+          onChangeUsername={onChangeUsername}
+          onChangePassword={onChangePassword}
+          handleLogout={handleLogout}
+          handleLogin={handleLogin}
+          handleSignup={handleSignup}
+          email={email}
+          password={password}
+        />
+      )}
+    </div>
+  );
+};
+
+// export default withStyles(styles, { withTheme: true })(HomePage);
+export default HomePage;
