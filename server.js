0c93a504d464671c851f517917167175ad565631
shree675 <shreeteshm37@gmail.com>
2021-06-23 14:03:53 +0530

    added more details in details page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config({ silent: true });
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Server running on port: %d', port);
